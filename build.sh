c84178842786f145a2c6300d5608e711373c7165
Christian U <info@cu-tec.de>
2017-07-02 17:07:18 +0200

    Add synapse dependency

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 cd $(dirname "$0")
 git submodule sync --recursive
 git submodule update --init --recursive
+lazbuild components/synapse/laz_synapse.lpk
 lazbuild components/general/general.lpk
 lazbuild source/avrusblaboszi.lpi
